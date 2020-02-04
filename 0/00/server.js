449fe745f8c92e075f9da53abba13ea82f7880c5
JoshuaHolloway <jhollow6@asu.edu>
2020-02-03 20:28:21 -0600

    removed 00

--- 0/00/server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static('public'));
-
-const port_num = 2222;
-app.listen(port_num, () => console.log('http://localhost:2222'));
